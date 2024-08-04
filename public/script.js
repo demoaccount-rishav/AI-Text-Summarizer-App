8da2480aa07d361e5217ea28e5a0f69c3215f5ee
demoaccount-rishav <rishavbhowmick.2002.va@gmail.com>
2024-08-05 01:13:57 +0530

    first commit

---
+++ public/script.js
@@ -0,0 +1,49 @@
+const textArea = document.getElementById("text_to_summarize");
+
+const submitButton = document.getElementById("submit-button");
+
+const summarizedTextArea = document.getElementById("summary");
+
+submitButton.disabled = true;
+
+function verifyTextLength(e) {
+
+  const text = e.target.value;
+
+  submitButton.disabled = (text.length > 200 && text.length < 100000) ? false : true;
+
+}
+
+function submitData(e) {
+
+  submitButton.classList.add("submit-button--loading");
+
+  const text_to_summarize = textArea.value;
+
+  // INSERT CODE SNIPPET FROM POSTMAN BELOW
+  const myHeaders = new Headers();
+  myHeaders.append("Content-Type", "application/json");
+
+  const raw = JSON.stringify({
+    "text_to_summarize": text_to_summarize
+  });
+
+  const requestOptions = {
+    method: "POST",
+    headers: myHeaders,
+    body: raw,
+    redirect: "follow"
+  };
+
+  fetch("/summarize", requestOptions)
+    .then((response) => response.text())
+    .then((result) => {
+      summarizedTextArea.value = result;
+      submitButton.classList.remove("submit-button--loading");
+    })
+    .catch((error) => console.error(error));
+}
+
+textArea.addEventListener("input", verifyTextLength);
+
+submitButton.addEventListener("click", submitData);
